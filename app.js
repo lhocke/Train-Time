5a1837f091a19a004d956eafef5dadfca221d754
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-27 16:13:53 -0700

    finishing html, starting js

---
+++ app.js
@@ -0,0 +1,13 @@
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDRuZ84r0hML2QgPe5iPdGfdBjyMYk-N-c",
+  authDomain: "ride-a-train-143cc.firebaseapp.com",
+  databaseURL: "https://ride-a-train-143cc.firebaseio.com",
+  projectId: "ride-a-train-143cc",
+  storageBucket: "ride-a-train-143cc.appspot.com",
+  messagingSenderId: "561049671726"
+};
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
