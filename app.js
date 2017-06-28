c0e1a506d940c29c29a03c899c6573cac7c5c523
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-27 21:44:48 -0700

    pushing completed app.js after too long between commits

--- app.js
+++ app.js
@@ -11,3 +11,62 @@ firebase.initializeApp(config);
 
 var database = firebase.database();
 
+$('#submit-button').on('click', function(){
+  event.preventDefault();
+
+  var newRoute = $('#train-route').val().trim();
+  var newDestination = $('#train-destination').val().trim();
+  var newStart = $('#train-start').val().trim();
+  console.log(newStart);
+  var newFrequency = $('#train-frequency').val().trim();
+
+  database.ref().push({
+    route : newRoute,
+    destination : newDestination,
+    start : newStart,
+    frequency : newFrequency,
+    startedAt : firebase.database.ServerValue.TIMESTAMP
+  })
+
+  console.log('attempt to push')
+
+})
+
+database.ref().on('child_added', function(snap){
+  console.log(snap.val().start)
+  var routeInfo = $('<tr>');
+  var displayRoute = $('<td>').append(snap.val().route);
+  var displayDestination = $('<td>').append(snap.val().destination);
+  var displayFrequency = $('<td>').append(snap.val().frequency);
+  var startTime = moment(snap.val().start,"HH:mm")
+  console.log(startTime)
+  
+  var currentTime = moment()
+  console.log(currentTime)
+
+  var frequencyUpdate = moment(startTime).subtract(1, "days");
+  var currentTime = moment();
+  var timeDifference = moment().diff(moment(frequencyUpdate), "minutes");
+  var timeRemainder = timeDifference % snap.val().frequency;
+  var timeUntil = snap.val().frequency - timeRemainder;
+  var next = moment().add(timeUntil, "minutes")
+
+  // if (moment(currentTime).isBefore(startTime)){
+  //   next = startTime;
+  //   console.log(next)
+  // }
+
+  // else {
+  //   next = moment(startTime).toNow()
+  // }
+  console.log(next)
+  
+  var displayNext = $('<td>').append(moment(next).format("h:mm a"));
+  var displayMinutes = $('<td>').append(timeUntil);
+
+
+
+  routeInfo.append(displayRoute, displayDestination, displayFrequency, displayNext, displayMinutes)
+  console.log(routeInfo)
+  $('#schedule').append(routeInfo)
+})
