f77decd2c763dd080efd666638e2fce5aa3a5f3d
Dylan Ishihara <dylan.ishihara@gmail.com>
2017-06-29 10:05:17 -0700

    new schedule experiment

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ $('#submit-button').on('click', function(event){
   $('input').val('')
 
 })
-
+// updates start time at mignight each day
 if (currentTime === moment().startOf("day")){
   database.ref("child", function(snap){
     var startReset = moment(snap.val().start).add(1, 'd');
@@ -51,21 +51,23 @@ if (currentTime === moment().startOf("day")){
 database.ref().on('child_added', buildSchedule),function(err){
   console.log(err.code)};
 
-database.ref().on('child_changed', buildSchedule),function(err){
-  console.log(err.code);}
+if (currentTime === moment().startOf('minute')){
+  database.ref("child", buildSchedule)
+}
+// database.ref().on('child_changed', buildSchedule),function(err){
+//   console.log(err.code);}
 
+// take information stored in database and create table
 function buildSchedule(snap, prevChildKey){
   var routeInfo = $('<tr>');
   var displayRoute = $('<td>').append(snap.val().route);
   var displayDestination = $('<td>').append(snap.val().destination);
   var displayFrequency = $('<td>').append(snap.val().frequency);
-  var startTime = moment(snap.val().start,"HH:mm")
+  var startTime = moment(snap.val().start,"HH:mm");
   // console.log(moment(snap.val().start))
   // console.log(startTime)
-
-  // startTime = moment()
   
-  var currentTime = moment()
+  var currentTime = moment();
 
   if (currentTime.isBefore(startTime)){
     var next = startTime;
@@ -79,22 +81,15 @@ function buildSchedule(snap, prevChildKey){
     var timeRemainder = timeDifference % snap.val().frequency;
     var timeUntil = snap.val().frequency - timeRemainder;
     next = moment().add(timeUntil, "minutes");
-    
-    // console.log(next)
-    // console.log("second")
 
   }
-
-
-
-
   
   var displayNext = $('<td>').append(moment(next).format("h:mm a"));
   var displayMinutes = $('<td>').append(timeUntil);
 
 
 
-  routeInfo.append(displayRoute, displayDestination, displayFrequency, displayNext, displayMinutes)
+  routeInfo.append(displayRoute, displayDestination, displayFrequency, displayNext, displayMinutes);
   // console.log(routeInfo)
   $('#schedule').append(routeInfo);
   // setInterval(buildSchedule, 30000);
